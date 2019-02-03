fc39a858ea01bba5e879f92afa9b0f7724c90b7a
Zach Newton <znewton13@gmail.com>
2019-02-02 22:00:08 -0700

    Try to update map location based on long lat sliders

--- public/script.js
+++ public/script.js
@@ -1,30 +1,102 @@
+const debounce = (callback, wait) => {
+  let timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, wait);
+  };
+};
+
 require([
   'esri/Map',
   'esri/views/SceneView',
-  'esri/layers/TileLayer',
+  'esri/layers/FeatureLayer',
+  'esri/widgets/Locate',
   'dojo/domReady!',
-], function(Map, MapView, TileLayer) {
-  var transportationLayer = new TileLayer({
-    url:
-      'https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer',
-    id: 'streets',
-    opacity: 0.7,
+], function(Map, SceneView, FeatureLayer, Locate) {
+  const autoTheftDensityUrl =
+    'https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/Auto%20Theft%20Density/FeatureServer';
+  const bikeParkingUrl =
+    'https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/Bike%20Parking/FeatureServer';
+  const crashDataUrl =
+    'https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/crash_data/FeatureServer';
+  const trailheadDataUrl =
+    'https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer';
+  var featureLayer = new FeatureLayer({
+    url: trailheadDataUrl,
   });
 
   var map = new Map({
-    basemap: 'gray-vector',
+    basemap: 'streets-navigation-vector',
   });
 
-  var view = new MapView({
+  map.add(featureLayer);
+
+  var view = new SceneView({
     container: 'root',
     map: map,
-    camera: {
-      tilt: 0,
-      position: {
-        x: -93.204087,
-        y: 44.935271,
-        z: 2500,
-      },
+  });
+
+  let longitude = -118.71511;
+  let latitude = 34.09042;
+
+  view.when(function() {
+    view.goTo({
+      center: [longitude, latitude],
+      zoom: 10,
+      tilt: 70,
+    });
+  });
+
+  var locate = new Locate({
+    view: view,
+    useHeadingEnabled: false,
+    goToOverride: function(view, options) {
+      options.target.scale = 1500;
+      return view.goTo(options.target);
     },
   });
+
+  view.ui.add(locate, 'top-left');
+
+  const latSlider = document.getElementById('lat-slide');
+  latSlider.value = latitude;
+  const latVal = document.getElementById('lat-val');
+  const displayLat = lat =>
+    (latVal.innerHTML = `${Number(lat).toFixed(4)}&deg;`);
+
+  const longSlider = document.getElementById('long-slide');
+  longSlider.value = longitude;
+  const longVal = document.getElementById('long-val');
+  const displayLong = long =>
+    (longVal.innerHTML = `${Number(long).toFixed(4)}&deg;`);
+
+  displayLat(latitude);
+  displayLong(longitude);
+
+  const longLatUpdate = debounce(() => {
+    console.log('view update');
+    view.when(function() {
+      view.goTo({
+        center: [longitude, latitude],
+        zoom: 10,
+        tilt: 70,
+      });
+    });
+  }, 300);
+
+  const handleLatChange = e => {
+    const value = e.target.value;
+    displayLat(value);
+    latitude = value;
+    longLatUpdate();
+  };
+  const handleLongChange = e => {
+    const value = e.target.value;
+    displayLong(value);
+    longitude = value;
+    longLatUpdate();
+  };
+
+  latSlider.addEventListener('input', handleLatChange);
+  longSlider.addEventListener('input', handleLongChange);
 });
